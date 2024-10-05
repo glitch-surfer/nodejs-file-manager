3abed58cd69a1c30921b217e421b0322fa163786
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-05 22:14:59 +0400

    feat/fix: add exit with goodbye handler for .exit & ctrl+c paths

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 import * as os from "node:os";
+import * as readline from "node:readline";
 
 const USERNAME_KEY = '--username=';
 const userName = process.argv.filter(arg => arg.includes(USERNAME_KEY))[0]?.replace(USERNAME_KEY, '') ?? 'Default Username';
 let workingDirectory = os.homedir();
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 const printLocation = () => console.log(`You are currently in ${workingDirectory}`);
+const goodBye = () => console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+const exit = () => {
+    goodBye();
+    rl.close();
+    process.exit();
+};
+
 
 console.log(`Welcome to the File Manager, ${userName}!`)
 
 printLocation();
 
-process.once('beforeExit', () => console.log(`Thank you for using File Manager, ${userName}, goodbye!`))
+// process.once('exit', () => goodBye())
+rl.on('SIGINT', exit);
+
+const promptUser = () => {
+    rl.question('', (input) => {
+        const command = input.trim();
+
+        if (command === '.exit') {
+            exit();
+        } else {
+            promptUser();
+        }
+    });
+}
+
+promptUser();
