2183e7d5aed1b29376d63ba3601e12f9408b47fb
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-05 21:34:43 +0400

    feat: add username greetings

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('hello')
+const USERNAME_KEY = '--username=';
+const userName = process.argv.filter(arg => arg.includes(USERNAME_KEY))[0]?.replace(USERNAME_KEY, '') ?? 'Default Username';
+
+console.log(`Welcome to the File Manager, ${userName}!`)
