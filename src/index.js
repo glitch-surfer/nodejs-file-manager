e28b8ed0b428f3f253e2f895af1fd5e65f004127
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-05 21:48:29 +0400

    feat: add working directory message & starts from homeDir

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import * as os from "node:os";
+
 const USERNAME_KEY = '--username=';
 const userName = process.argv.filter(arg => arg.includes(USERNAME_KEY))[0]?.replace(USERNAME_KEY, '') ?? 'Default Username';
+let workingDirectory = os.homedir();
+
+const printLocation = () => console.log(`You are currently in ${workingDirectory}`);
 
 console.log(`Welcome to the File Manager, ${userName}!`)
+
+printLocation();
+
 process.once('beforeExit', () => console.log(`Thank you for using File Manager, ${userName}, goodbye!`))
