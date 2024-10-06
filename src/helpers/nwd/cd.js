d224a8c0842bab35bcd99ddc57ef15edc3bc2d24
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-06 09:42:13 +0400

    feat: implement cd

---
+++ src/helpers/nwd/cd.js
@@ -0,0 +1,19 @@
+import path from "path";
+import {up} from "./up.js";
+import * as fs from "node:fs";
+
+export const cd = async (pathToNavigate, workingDirectory) => {
+    if (pathToNavigate.includes('..') && workingDirectory.path === workingDirectory.systemRoot) return;
+
+    if (pathToNavigate === '..') {
+        up(workingDirectory);
+        return;
+    }
+
+    const newPath = path.join(workingDirectory.path, pathToNavigate);
+    if (!fs.existsSync(newPath) || (await fs.promises.stat(newPath)).isFile()) {
+        throw new Error('No such directory')
+    }
+
+    workingDirectory.path = newPath;
+}
