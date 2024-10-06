b3c851be2de787d264c335fa2102436cc7756543
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-06 21:33:42 +0400

    fix/refactor: cp command (allow use path to directory with or without new file name)

--- src/helpers/fs/copy.js
+++ src/helpers/fs/copy.js
@@ -1,22 +1,23 @@
 import fs from 'fs';
 import {getPathFromArg} from "../get-path-from-arg.js";
 import {pipeline} from "stream/promises";
+import path from "path";
 
 export const copy = async (sourceFileName, destinationFolderName, workingDirectory) => {
     const sourceFolderPath = getPathFromArg(sourceFileName, workingDirectory);
     const destinationFolderPath = getPathFromArg(destinationFolderName, workingDirectory);
 
     const isSourceFileExists = await fs.promises.stat(sourceFolderPath).then((file) => file.isFile() && true).catch(() => false);
-    const isDestinationFolderExists = await fs.promises.stat(destinationFolderPath).then((folder) => folder.isDirectory() && true).catch(() => false);
+    const isDestinationPathFolder = await fs.promises.stat(destinationFolderPath).then((file) => file.isDirectory() && true)
 
-    if (isDestinationFolderExists || !isSourceFileExists) {
+    if (!isSourceFileExists) {
         throw new Error('Should specify a correct file names');
     }
 
     try {
         await pipeline(
             fs.createReadStream(sourceFolderPath),
-            fs.createWriteStream(destinationFolderPath)
+            fs.createWriteStream(isDestinationPathFolder ? path.join(destinationFolderPath, sourceFileName) : destinationFolderPath)
         )
     } catch (error) {
         throw new Error(error);
